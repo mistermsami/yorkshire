3db51abcfaa36ecf65b9317c87541d6436b28ed1
kamranafridi123 <kamranafridi089@gmail.com>
2025-05-29 13:25:48 +0100

    comite

--- app/Models/ALLAppointment.php
+++ app/Models/ALLAppointment.php
@@ -9,5 +9,5 @@ class ALLAppointment extends Model
     protected $table = 'all_appointment';
     protected $fillable = ['name', 'email', 'contact', 'date', 'appointment_type', 'message', 'paid', 'pirce']; // Fillable fields for mass assignment
     //
-    public $timestamps = true; // This ensures Laravel adds created_at and updated_at
+     public $timestamps = true; // This ensures Laravel adds created_at and updated_at
 }
