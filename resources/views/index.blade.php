19129abfa9c4333890724d6bebd86d97c89b8938
kamranafridi123 <kamranafridi089@gmail.com>
2025-06-07 18:16:24 +0100

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1055,7 +1055,6 @@
             <div class="container">
                 <div class="trusted-men-header">
                     <h2>More than <span class="trusted-men-counter">100,000</span> clients<br>trust our treatment plans</h2>
-                    <h2>More than <span class="trusted-men-counter">100,000</span> people<br>trust our treatment plans</h2>
                 </div>
 
 
