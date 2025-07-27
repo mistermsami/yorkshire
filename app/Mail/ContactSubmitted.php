50b9763bf7b512b5c182531fef723e790ab32e27
kamranafridi123 <kamranafridi089@gmail.com>
2025-07-27 22:11:56 +0100

    COMMIT

--- app/Mail/ContactSubmitted.php
+++ app/Mail/ContactSubmitted.php
@@ -22,16 +22,16 @@ class ContactSubmitted extends Mailable
 
     public function build()
     {
-        Log::info("Appointment Confirmation Message", [
-            'name' => $this->emailform->name,   
-            'email' => $this->emailform->email,
-            'contact' => $this->emailform->contact,
-            'appointment_type' => $this->emailform->appointment_type,
-            'date' => $this->emailform->date,
-            'time' => $this->emailform->time,
-            'message' => $this->emailform->message,
-            'pagetype' => $this->pagetype,
-        ]);
+        // Log::info("Appointment Confirmation Message", [
+        //     'name' => $this->emailform->name,   
+        //     'email' => $this->emailform->email,
+        //     'contact' => $this->emailform->contact,
+        //     'appointment_type' => $this->emailform->appointment_type,
+        //     'date' => $this->emailform->date,
+        //     'time' => $this->emailform->time,
+        //     'message' => $this->emailform->message,
+        //     'pagetype' => $this->pagetype,
+        // ]);
 
         if ($this->pagetype == '1') {
             return $this->from('noreply@yorkshirehairandskinsolutions.co.uk', 'Yorkshire')
@@ -44,6 +44,9 @@ class ContactSubmitted extends Mailable
                 ->cc('support@yorkshirehairandskinsolutions.co.uk')
                 ->view('emails.appointment-submitted');
         }
-        dd(($this->pagetype));
+        return $this->from('noreply@yorkshirehairandskinsolutions.co.uk', 'Yorkshire')
+            ->subject('Thank you for contacting Yorkshire Hair & Skin Solutions')
+            ->cc('support@yorkshirehairandskinsolutions.co.uk')
+            ->view('emails.appointment-submitted');
     }
 }
