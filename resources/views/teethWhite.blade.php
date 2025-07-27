88e6e61f52f41a03a382f8662b59127d0ee760f1
kamranafridi123 <kamranafridi089@gmail.com>
2025-07-27 23:10:36 +0100

    commit

--- resources/views/teethWhite.blade.php
+++ resources/views/teethWhite.blade.php
@@ -983,14 +983,7 @@
             }
 
             createHydraFacialBubbles();
-            const bookButtons = document.getElementsByClassName('bookNow');
-
-            // If there's only one button, access the first one
-            // bookButtons[0].addEventListener('click', function(e) {
-            //     e.preventDefault();
-            //     const select = document.getElementById('appointmentSelect');
-            //     select.value = "Signature HydraFacial"; // Value must match the option text exactly
-            // });
+            const bookButtons = document.getElementsByClassName('bookNow'); 
             document.querySelectorAll('.bookNow').forEach(function(button) {
                 button.addEventListener('click', function(e) {
                     e.preventDefault(); // Prevent default action
